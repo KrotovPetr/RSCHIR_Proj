2e86c913a751c73e6312b796594cffe1e45b9b6e
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-11-25 14:47:45 +0300

    Modify to v6.1

---
+++ apache/pages/admin/graphics/getFixtures.php
@@ -0,0 +1,15 @@
+<?php
+include '../../api/services/login.php';
+require '../../../vendor/autoload.php';
+use Nelmio\Alice\Loader\NativeLoader;
+
+
+class Dump {
+    public $squirrels;
+    public $fats;
+    public $carbohydrates;
+}
+
+
+$loader = new Nelmio\Alice\Loader\NativeLoader();
+$fixtures = $loader->loadFile('./fixtures.yml');
---
+++ apache/pages/admin/graphics/index.php
@@ -0,0 +1,34 @@
+<?php
+include '../login.php';
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Графики</title>
+</head>
+
+<body>
+    <h1>Графики</h1>
+
+    <div>
+        <a href="../admin.php">административная панель</a>
+    </div>
+
+    <div>
+        <img src="./graphicSquirrelsFatsCarbohydrates.php" />
+    </div>
+    <div>
+        <img src="./graphicSquirrels.php" />
+        <img src="./graphicFats.php" />
+    </div>
+    <div>
+        <img src="./graphicCarbohydrates.php" />
+    </div>
+</body>
+
+</html>
